ffb143bf39afdff7c7bd81d99018983f8a95ca71
liphr <liphr@DESKTOP-PMVORIV>
2022-04-01 19:10:07 +0300

    Add project files.

---
+++ archiveTool/Options.cs
@@ -0,0 +1,26 @@
+﻿using CommandLine;
+
+namespace archiveTool
+{
+    public class Options
+    {
+        [Option('d', "directory", Required = false, HelpText = "The path of the Directory you want to archive.")]
+        public string Directory { get; set; }
+
+        [Option('i', "iga", Required = false, HelpText = "The path of the desired igArchive.")]
+        public string ArchivePath { get; set; }
+
+        [Option('p', "path", Required = true, HelpText = "The path of where you want to write an igArchive or extract files from an igArchive.")]
+        public string Path { get; set; }
+
+        [Option('c', "compression", Default = 2, Required = false, HelpText = "The desired compression mode for writing an igArchive: " +
+            "\n0: No compression." +
+            "\n1: Zlib compression (average compression ratio, average decompression performance)." +
+            "\n2: Lzma compression (best compression ratio, slowest decompression performance)." +
+            "\n3: Lz4 compression (poorest compression ratio, fastest decompression performance).")]
+        public int CompressionMode { get; set; }
+
+        [Option('l', "logicalName", Default = true, Required = false, HelpText = "Use the logical name of the files when extracting from an igArchive.")]
+        public bool UseLogicalName { get; set; }
+    }
+}
---
+++ archiveTool/Program.cs
@@ -0,0 +1,73 @@
+﻿using CommandLine;
+using igArchiveLib;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+
+namespace archiveTool
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Parser.Default.ParseArguments<Options>(args).WithParsed(ProcessOptions);
+        }
+
+        static void ProcessOptions(Options options)
+        {
+            if (options.Directory == null && options.ArchivePath == null)
+                throw new Exception("No mode specified");
+            if (options.Directory != null && options.ArchivePath != null)
+                throw new Exception("Can't have directory and archive at the same time!");
+
+            if (options.ArchivePath != null)
+                ProcessIGA(options.ArchivePath, options.Path, options.UseLogicalName);
+            else
+                ProcessWrite(options.Directory, options.Path, options.CompressionMode);
+        }
+
+        static void ProcessIGA(string ArchivePath, string ExtractTo, bool UseLogicalName = true)
+        {
+            if (!File.Exists(ArchivePath))
+                throw new Exception("igArchive doesn't exist!");
+
+            Console.WriteLine("Opening igArchive.");
+
+            igArchive archive = new igArchive();
+            archive.open(ArchivePath);
+            
+            Console.WriteLine("Extracting Files.");
+
+            Stopwatch watch = Stopwatch.StartNew();
+
+            for(int i = 0; i < archive._fileInfoTable.Length; i++)
+            {
+                string DesiredPath = Path.Combine(ExtractTo, UseLogicalName ? archive._logicalNameTable[i] : archive._nameTable[i]);
+                Directory.CreateDirectory(Path.GetDirectoryName(DesiredPath));
+                File.WriteAllBytes(DesiredPath, archive.read(archive._fileInfoTable[i]));
+            }
+            archive.close();
+            watch.Stop();
+
+            Console.WriteLine("Archive extraction complete!");
+            Console.WriteLine("Time elapsed: {0}s", watch.Elapsed.TotalSeconds);
+        }
+
+        static void ProcessWrite(string Directory, string ArchivePath, int CompressionMode)
+        {
+            if (CompressionMode > 3 || CompressionMode < 0)
+                throw new Exception("Invalid Compression Mode!");
+
+            Console.WriteLine("Creating igArchive.");
+            igArchive archive = new igArchive();
+            archive.createFromDirectory(ArchivePath, Path.GetDirectoryName(Directory), (igArchive.CompressionType)CompressionMode, PercentageReport: (percentage) =>
+            {
+                Console.SetCursorPosition(0, Console.CursorTop);
+                Console.Write(new string(' ', Console.WindowWidth));
+                Console.Write("\rProcessed: {0}% of Files.", (int)(percentage * 100f));
+            });
+            Console.WriteLine();
+            Console.WriteLine("igArchive has been created sucessfully.");
+            archive.close();
+        }
+    }
+}
